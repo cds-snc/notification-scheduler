57a2189df8e7a4f102eea6fdd64bf1e1954ed8e0
Tim Arney <tim@line37.com>
2019-12-30 11:25:05 -0500

    add days of the week

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { yearMonthDay, getDates } from "./util";
 import { Days } from "./Days";
+import { DaysOfTheWeek } from "./DaysOfTheWeek";
 import { StateProvider } from "../store.js";
 
 export const Calendar = () => {
   const weeks = getDates();
   return (
-    <section id="Calendar-dates" aria-label="Calendar dates" role="application">
-      {weeks.map(week => {
-        return (
-          <div key={yearMonthDay(week[0])} className="Calendar-row">
-            <Days week={week} />
-          </div>
-        );
-      })}
-    </section>
+    <div className="Calendar-grid">
+      <DaysOfTheWeek />
+      <section
+        id="Calendar-dates"
+        aria-label="Calendar dates"
+        role="application"
+      >
+        {weeks.map(week => {
+          return (
+            <div key={yearMonthDay(week[0])} className="Calendar-row">
+              <Days week={week} />
+            </div>
+          );
+        })}
+      </section>
+    </div>
   );
 };
 
