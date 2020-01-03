6d40b15efba60efabcde40af9aa33f52a0e96430
Tim Arney <tim@line37.com>
2020-01-03 14:06:48 -0500

    update styles

--- components/Toggle/Toggle.js
+++ components/Toggle/Toggle.js
@@ -5,14 +5,28 @@ export const Toggle = () => {
   const { _24hr, dispatch } = useContext(store);
   return (
     <div className="toggle-holder">
-      <div className="toggle-label">am/pm</div>
+      <div
+        className={[
+          "toggle-label",
+          _24hr === "off" ? "toggle-label-active" : ""
+        ].join(" ")}
+      >
+        am/pm
+      </div>
       <button
         className={`switch ${_24hr}`}
         onClick={() => {
           dispatch({ type: "AM_PM", payload: _24hr });
         }}
       />
-      <div className="toggle-label">24h</div>
+      <div
+        className={[
+          "toggle-label",
+          _24hr === "on" ? "toggle-label-active" : ""
+        ].join(" ")}
+      >
+        24h
+      </div>
     </div>
   );
 };
