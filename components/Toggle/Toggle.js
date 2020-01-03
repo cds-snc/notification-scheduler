7b2f5d1585aab5021ff2f7286367c6651345338e
Tim Arney <tim@line37.com>
2020-01-03 11:49:00 -0500

    mod toggle to be a button

--- components/Toggle/Toggle.js
+++ components/Toggle/Toggle.js
@@ -6,7 +6,7 @@ export const Toggle = () => {
   return (
     <div className="toggle-holder">
       <div className="toggle-label">am/pm</div>
-      <div
+      <button
         className={`switch ${_24hr}`}
         onClick={() => {
           dispatch({ type: "AM_PM", payload: _24hr });
