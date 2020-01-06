aed4040adf81b3321d9f3aa69ef92e1df6d11644
Tim Arney <tim@line37.com>
2020-01-06 15:29:45 -0500

    add set date and time

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./scheduler/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("schedule-send-at"));
