28509060498ee15df3d17b9e474135dc328a7f15
Tim Arney <tim@line37.com>
2020-01-06 10:50:35 -0500

    move to App

--- main.js
+++ main.js
@@ -1,32 +1,4 @@
-import ReactDOM from "react-dom";
 import React from "react";
-import { StateProvider } from "./components/_store";
-import { Calendar } from "./components/Calendar/Calendar";
-import { Date } from "./components/Date/Date";
-import { Toggle } from "./components/Toggle/Toggle";
-import { Time } from "./components/Time/Time";
-import "./style.css";
-
-export const App = () => {
-  return (
-    <StateProvider>
-      <div className="schedule">
-        <div>
-          <Calendar />
-        </div>
-        <div className="column">
-          <div className="selected-date-time-box">
-            <div className="triangle"></div>
-            <div className="date-time-box">
-              <Date />
-              <Time name="time" />
-              <Toggle />
-            </div>
-          </div>
-        </div>
-      </div>
-    </StateProvider>
-  );
-};
-
+import ReactDOM from "react-dom";
+import { App } from "./components/App";
 ReactDOM.render(<App />, document.getElementById("root"));
