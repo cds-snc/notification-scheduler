1f42e5093ea72e1b6a2bdd2330fea096d659987c
Tim Arney <tim@line37.com>
2020-01-06 11:28:51 -0500

    update dir

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./components/App";
+import { App } from "./scheduler/App";
+
 ReactDOM.render(<App />, document.getElementById("root"));
