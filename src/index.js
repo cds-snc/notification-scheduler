1dc10313a2054e784b3947bf3520cf55f33954ef
Tim Arney <tim@line37.com>
2020-02-06 10:06:18 -0500

    add dayjs to window

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./scheduler/App";
+import dayjs from "dayjs";
+window.dayjs = dayjs;
 
 ReactDOM.render(<App />, document.getElementById("schedule-send-at"));
