4bb16ff4396895ae0873b0b2b9c300b568d97bef
smcmurtry <smcmurtry@gmail.com>
2020-02-19 20:39:29 -0700

    removed console logs

--- src/scheduler/Calendar/_util.js
+++ src/scheduler/Calendar/_util.js
@@ -4,7 +4,6 @@ let options = {};
 
 options = { ...window.schedulerOptions };
 export const isBlockedDay = options.init(dayjs).isBlockedDay;
-console.log("options", options)
 
 export const getFirstAvailableDay = (day, state) => {
   if (dayjs(day).isSame(state.firstAvailableDate)) {
